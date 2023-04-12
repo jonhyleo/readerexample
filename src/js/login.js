dabc3be7895e28a2e3c3be7a89130fcfc20321e1
Jonhy Sanchez <jonhy.sanchez@betterdigital.com>
2023-04-12 15:48:07 -0300

    form reset

--- src/js/login.js
+++ src/js/login.js
@@ -8,5 +8,8 @@ loginBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const username = form.querySelector('.username').value;
   const password = form.querySelector('.password').value;
+
   username && password && alert(`You have logged in with:\n\nUsername: ${username} Password: ${password}`);
+
+  form.reset();
 });
